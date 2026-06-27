a162b6cb18c32ad16a608c76aa5a917638ed1755
S0LTIC3 <88272165+S0LTIC3@users.noreply.github.com>
2026-06-28 01:24:57 +0200

    chore: initialisation du projet

---
+++ src/test/java/fr/pharmelys/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.pharmelys.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
